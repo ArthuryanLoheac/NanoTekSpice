2f8ffbbaef8e07033845cc2c8bec56f8a8b374a9
mathys-f <mathys.fillion@epitech.eu>
2025-02-13 15:51:29 +0100

    🔥 Remove unnecessary initialization of _internComponents in AInput and ASingle constructors

--- src/AInput.cpp
+++ src/AInput.cpp
@@ -11,6 +11,5 @@ nts::AInput::AInput(std::string name) : AComponent(name)
 {
     _inOuts.push_back(std::make_pair(OUT,
         std::vector<std::pair<IComponent &, std::size_t>>()));
-    _internComponents = std::vector<IComponent>();
     _lastValueComputed = UNDEFINED;
 }
