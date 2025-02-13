beec540cef48175a4065fcb5935282f2da6fc1ea
mathys-f <mathys.fillion@epitech.eu>
2025-02-13 16:59:15 +0100

    ✨ Add ComponentInput class implementation

---
+++ src/Component/ComponentInput.cpp
@@ -0,0 +1,29 @@
+/*
+** EPITECH PROJECT, 2025
+** ComponentInput.cpp
+** File description:
+** ComponentInput.cpp
+*/
+
+#include "ComponentInput.hpp"
+
+nts::ComponentInput::ComponentInput(std::string name)
+    : AInput(name)
+{
+    _value = nts::UNDEFINED;
+}
+
+nts::ComponentInput::~ComponentInput()
+{
+}
+
+nts::Tristate nts::ComponentInput::compute(std::size_t pin)
+{
+    (void)pin;
+    return _value;
+}
+
+void nts::ComponentInput::setValue(nts::Tristate value)
+{
+    _value = value;
+}
