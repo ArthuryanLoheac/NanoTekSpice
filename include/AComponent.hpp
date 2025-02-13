0d3be3bf224acfd1db0229070ad186cb45c08c41
Mathys Fillion <146719174+mathys-f@users.noreply.github.com>
2025-02-13 14:18:58 +0100

    🔀 Implement AComponent
    * ✨ Implement AComponent class and add getInOut to IComponent interface
    
    * ✨ Change IComponent references to IComponent& in getInOut method and related areas

---
+++ include/AComponent.hpp
@@ -0,0 +1,46 @@
+/*
+** EPITECH PROJECT, 2025
+** AComponent.hpp
+** File description:
+** AComponent.hpp
+*/
+
+#ifndef A_COMPONENT_HPP
+#define A_COMPONENT_HPP
+
+#include <string>
+#include <map>
+#include <vector>
+
+#include "IComponent.hpp"
+
+namespace nts
+{
+    class AComponent : public IComponent
+    {
+    public:
+        virtual ~AComponent() = default;
+        virtual void simulate(std::size_t tick) override;
+        virtual nts::Tristate compute(std::size_t pin) override;
+        virtual void setLink(std::size_t pinOut, nts::IComponent &other,
+                            std::size_t pinIn) override;
+        virtual size_t pinOutToInternPin(size_t pin) override;
+        virtual void setNotComputed() override;
+        virtual size_t getIdFromPin(size_t pin) override;
+        std::vector<std::pair<TypePin,
+            std::vector<std::pair<IComponent &,
+            std::size_t>>>> getInOut() override;
+
+    protected:
+        AComponent(std::string name)
+            : _name(name) {};
+        std::string _name;
+        std::vector<std::pair<TypePin,
+            std::vector<std::pair<IComponent &,
+            std::size_t>>>> _inOuts;
+        std::vector<IComponent> _internComponents;
+        Tristate _lastValueComputed;
+    };
+}
+
+#endif /* !A_COMPONENT_HPP */
---
+++ src/AComponent.cpp
@@ -0,0 +1,54 @@
+/*
+** EPITECH PROJECT, 2025
+** AComponent.cpp
+** File description:
+** AComponent.cpp
+*/
+
+#include "AComponent.hpp"
+
+void nts::AComponent::simulate(std::size_t tick)
+{
+    tick = tick;
+}
+
+nts::Tristate nts::AComponent::compute(std::size_t pin)
+{
+    pin = pin;
+    _lastValueComputed = UNDEFINED;
+    return _lastValueComputed;
+}
+
+void nts::AComponent::setLink(std::size_t pinOut, nts::IComponent &other,
+                              std::size_t pinIn)
+{
+    if (_internComponents.size() > 0) {
+        setLink(pinOut, _internComponents[getIdFromPin(pinOut)],
+            _internComponents[getIdFromPin(pinOut)].pinOutToInternPin(pinOut));
+    }
+    _inOuts[pinOut].second.push_back(std::make_pair(std::ref(other), pinIn));
+    other.getInOut()[pinIn].second.push_back(std::make_pair(std::ref(*this), pinOut));
+}
+
+size_t nts::AComponent::pinOutToInternPin(size_t pin)
+{
+    return pin;
+}
+
+void nts::AComponent::setNotComputed()
+{
+    _lastValueComputed = NOTCOMPUTED;
+}
+
+size_t nts::AComponent::getIdFromPin(size_t pin)
+{
+    pin = pin;
+    return 0;
+}
+
+std::vector<std::pair<nts::TypePin,
+            std::vector<std::pair<nts::IComponent &,
+            std::size_t>>>> nts::AComponent::getInOut()
+{
+    return _inOuts;
+}
