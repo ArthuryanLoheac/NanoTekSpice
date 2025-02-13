0831dcd90ce631aa0efc1b6137cf8f9ed753b63a
Arthuryan Lohéac <146721013+ArthuryanLoheac@users.noreply.github.com>
2025-02-13 15:46:49 +0100

    🔀 Implement ASingle component

---
+++ src/ASingle.cpp
@@ -0,0 +1,24 @@
+/*
+** EPITECH PROJECT, 2025
+** NanoTekSpice
+** File description:
+** ASingle
+*/
+
+#include "ASingle.hpp"
+
+static std::pair<nts::TypePin, std::vector<std::pair<nts::IComponent &,
+    std::size_t>>> makePair(nts::TypePin type)
+{
+    return std::make_pair(type,
+        std::vector<std::pair<nts::IComponent &, std::size_t>>());
+}
+
+nts::ASingle::ASingle(std::string name) : AComponent(name)
+{
+    _inOuts.push_back(makePair(IN));
+    _inOuts.push_back(makePair(IN));
+    _inOuts.push_back(makePair(OUT));
+    _lastValueComputed = UNDEFINED;
+    _internComponents = std::vector<IComponent>();
+}
