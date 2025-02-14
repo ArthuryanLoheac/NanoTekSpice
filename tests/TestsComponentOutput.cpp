4bcde32b2d379b8159ba153eb5de10cf8db66aa8
Arthuryan Loheac <arthuryan.loheac-de-sainte-marie@epitech.eu>
2025-02-14 14:52:30 +0100

    ✅ Add tests to errors links

--- tests/TestsComponentOutput.cpp
+++ tests/TestsComponentOutput.cpp
@@ -55,3 +55,25 @@ Test(ComponentOutput, setNotComputed, .init=redirect_all_std)
     componentOutput.setNotComputed();
     cr_assert_eq(componentOutput.compute(0), nts::UNDEFINED);
 }
+
+Test(ComponentOutput, AlreadyLinked, .init=redirect_all_std)
+{
+    nts::ComponentOutput componentOutput("out");
+    nts::ComponentInput componentInput("in");
+    nts::ComponentInput componentInput2("in2");
+
+    componentOutput.setNotComputed();
+    componentInput.setLink(1, componentOutput, 1);
+    cr_assert_throw(componentInput2.setLink(1, componentOutput, 1), nts::AComponent::Errors);
+    cr_assert_throw(componentOutput.setLink(1, componentInput2, 1), nts::AComponent::Errors);
+}
+
+Test(ComponentOutput, PinUnexisting, .init=redirect_all_std)
+{
+    nts::ComponentOutput componentOutput("out");
+    nts::ComponentInput componentInput("in");
+
+    componentOutput.setNotComputed();
+    cr_assert_throw(componentInput.setLink(1, componentOutput, 21), nts::AComponent::Errors);
+    cr_assert_throw(componentInput.setLink(21, componentOutput, 1), nts::AComponent::Errors);
+}
