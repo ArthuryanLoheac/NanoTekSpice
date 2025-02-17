8051a51f6a887ba382916db962d94ef6d2c9d8c1
mathys-f <mathys.fillion@epitech.eu>
2025-02-17 03:32:55 +0100

    ✨ Add Component4071 with tests

---
+++ include/Component4071.hpp
@@ -0,0 +1,22 @@
+/*
+** EPITECH PROJECT, 2025
+** Component4071.hpp
+** File description:
+** Component4071.hpp
+*/
+
+#ifndef COMPONENT_4071_HPP
+#define COMPONENT_4071_HPP
+
+#include "AFourGates.hpp"
+
+namespace nts
+{
+    class Component4071 : public AFourGates
+    {
+        public:
+            Component4071(std::string name);
+    };
+}
+
+#endif /* !COMPONENT_4071_HPP */
