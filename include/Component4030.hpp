14ac3305a17058d415435e5a6d95cec915ac7b17
Arthuryan Loheac <arthuryan.loheac-de-sainte-marie@epitech.eu>
2025-02-17 10:44:08 +0100

    ✨ new Component 4030

---
+++ include/Component4030.hpp
@@ -0,0 +1,23 @@
+/*
+** EPITECH PROJECT, 2025
+** ComponentAnd.hpp
+** File description:
+** ComponentAnd.hpp
+*/
+
+#ifndef COMPONENT_4030_HPP
+#define COMPONENT_4030_HPP
+
+#include "AFourGates.hpp"
+#include "ComponentXor.hpp"
+
+namespace nts
+{
+    class Component4030 : public AFourGates
+    {
+        public:
+            Component4030(std::string name);
+    };
+}
+
+#endif /* !COMPONENT_4030_HPP */
